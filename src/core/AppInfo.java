fddd3b95491c3add9f94ebf47596bc6c59478e47
baltasarq <baltasarq@gmail.com>
2022-06-21 13:00:03 +0200

    Bumped version

--- src/core/AppInfo.java
+++ src/core/AppInfo.java
@@ -12,7 +12,7 @@ package core;
  */
 public class AppInfo {
     public final static String name = "Babble";
-    public final static String version = "0.40 20141002";
+    public final static String version = "0.41 20220621";
     public final static String author = "baltasarq@gmail.com";
     public final static String web = "http://caad.es/baltasarq/";
 }
